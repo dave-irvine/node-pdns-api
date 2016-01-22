eb6cdd5dcce53fa100f79787901338e6ddd73f20
Dave Irvine <dave@daveirvine.me>
2016-01-22 17:33:19 +0000

    Export module properly, this isn't an application

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-require('source-map-support').install();
-require('babel-polyfill');
-Promise = require('bluebird');
-
-Promise.longStackTraces();
-Promise.onPossiblyUnhandledRejection(function(error) {
-    throw error;
-});
-
-require('./lib/index');
+module.exports = require('./lib/index');
