5338369b23e1029e3ca8c0a15f945a0179b5a45c
Dave Irvine <dave@daveirvine.me>
2016-01-28 15:17:01 +0000

    Add list() to Zones

--- src/Zones.js
+++ src/Zones.js
@@ -1,9 +1,68 @@
+import inspector from 'schema-inspector';
+import Promise from 'bluebird';
+
 import dbg from 'debug';
 let debug = dbg('pdns-api:Zones');
 
 class Zones {
     constructor(connection) {
         debug(`constructor(${connection})`);
+
+        if (!connection) {
+            throw new Error('Expected Connection as constructor parameter');
+        }
+
+        this.connection = connection;
+    }
+
+    list() {
+        debug(`list()`);
+
+        let connection = this.connection,
+            err;
+
+        return new Promise((resolve, reject) => {
+            let url = `${connection.baseURL}${connection.zones_url.replace('{/zone}', '')}`;
+
+            connection.get(url)
+            .then((body) => {
+                let zones = body;
+
+                const zonesSchema = {
+                    type: 'array',
+                    items: [
+                        {
+                            type: 'object',
+                            properties: {
+                                id: { type: 'string' },
+                                name: { type: 'string' },
+                                url: { type: 'string' },
+                                kind: { type: 'string' },
+                                dnssec: { type: 'boolean' },
+                                account: { type: 'string' },
+                                masters: { type: 'array' },
+                                serial: { type: 'number' },
+                                notified_serial: { type: 'number' },
+                                last_check: { type: 'number' }
+                            }
+                        }
+                    ]
+                };
+
+                let result = inspector.validate(zonesSchema, zones);
+
+                if (!result.valid) {
+                    err = new Error(`API returned invalid results: \n\n${result.format()}`);
+
+                    throw err;
+                }
+
+                return resolve(zones);
+            })
+            .catch((e) => {
+                return reject(e);
+            });
+        });
     }
 }
 
---
+++ test/Zones.test.js
@@ -0,0 +1,139 @@
+import chai, {expect} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import proxyquire from 'proxyquire';
+import Promise from 'bluebird';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('Zones', () => {
+    let Connection,
+        Zones;
+
+    describe('constructor()', () => {
+        beforeEach(() => {
+            Zones = require('../lib/Zones');
+        });
+
+        it('should throw an error if not passed a Connection object', () => {
+            return expect(() => {
+                return new Zones();
+            }).to.throw('Expected Connection');
+        });
+
+        it('should store the Connection object locally', () => {
+            let expectedConnection = {
+                'test': 'abcd'
+            };
+
+            let zones = new Zones(expectedConnection);
+
+            return expect(zones.connection).to.deep.equal(expectedConnection);
+        });
+    });
+
+    describe('list()', () => {
+        const configuration = {
+            'host': 'abc',
+            'port': 8080,
+            'protocol': 'http',
+            'key': 'abcd'
+        };
+
+        let connection,
+            requestStub,
+            validServer,
+            validZone,
+            zones;
+
+        before(() => {
+            requestStub = sinon.stub();
+
+            Connection = proxyquire('../lib/Connection', {
+                'request': requestStub
+            });
+
+            Zones = require('../lib/Zones');
+
+            validServer = {
+                'type': 'Server',
+                'id': 'localhost',
+                'url': '/servers/localhost',
+                'daemon_type': 'authoritative',
+                'version': '3.4.7',
+                'config_url': '/servers/localhost/config{/config_setting}',
+                'zones_url': '/servers/localhost/zones{/zone}'
+            };
+
+            validZone = {
+                'id': 'test.net.',
+                'url': '/servers/localhost/zones/test.net.',
+                'name': 'test.net',
+                'kind': 'Master',
+                'dnssec': false,
+                'account': '',
+                'masters': [],
+                'serial': 1970010101,
+                'notified_serial': 0,
+                'last_check': 0
+            };
+        });
+
+        beforeEach((done) => {
+            requestStub.yields(null, null, [validServer]);
+
+            connection = new Connection(configuration);
+            zones = new Zones(connection);
+
+            connection.connect()
+            .then(() => {
+                done();
+            });
+        });
+
+        it('should return a Promise', () => {
+            return expect(zones.list()).to.be.an.instanceOf(Promise);
+        });
+
+        it('should connect to `/zones` endpoint to fetch server configuration', () => {
+            requestStub.yields(null, null, [validZone]);
+            let expectedURL = `${configuration.protocol}://${configuration.host}:${configuration.port}/servers/localhost/zones`;
+
+            return zones.list()
+            .then(() => {
+                return expect(requestStub).to.have.been.calledWith(sinon.match({
+                    url: expectedURL
+                }));
+            });
+        });
+
+        it('should use a GET request', () => {
+            let expectedMethod = 'GET';
+            requestStub.yields(null, null, [validZone]);
+
+            return zones.list()
+            .then(() => {
+                return expect(requestStub).to.have.been.calledWith(sinon.match({
+                    method: expectedMethod
+                }));
+            });
+        });
+
+        it('should reject if the server connected to does not return an expected result', () => {
+            requestStub.yields(null, null, null);
+
+            return expect(zones.list()).to.eventually.be.rejectedWith('API returned invalid results');
+        });
+
+        it('should resolve with the list of zones', () => {
+            requestStub.yields(null, null, [validZone, validZone, validZone]);
+
+            return zones.list()
+            .then((zoneList) => {
+                return expect(zoneList).to.deep.equal([validZone, validZone, validZone]);
+            });
+        });
+    });
+});
