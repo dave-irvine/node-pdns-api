7830915298504b0e95a0252adc229a9b1e15ccb0
Dave Irvine <dave@daveirvine.me>
2016-01-28 17:07:18 +0000

    Add list() to Records

--- src/Records.js
+++ src/Records.js
@@ -1,9 +1,61 @@
+import inspector from 'schema-inspector';
+import Promise from 'bluebird';
+
 import dbg from 'debug';
 let debug = dbg('pdns-api:Records');
 
 class Records {
     constructor(connection) {
         debug(`constructor(${connection})`);
+
+        if (!connection) {
+            throw new Error('Expected Connection as constructor parameter');
+        }
+
+        this.connection = connection;
+    }
+
+    list(zone) {
+        debug(`list()`);
+
+        let connection = this.connection,
+            err;
+
+        return new Promise((resolve, reject) => {
+            connection.zones.fetch(zone)
+            .then((zoneRecord) => {
+                let records = zoneRecord.records;
+
+                const recordsSchema = {
+                    type: 'array',
+                    items: [
+                        {
+                            type: 'object',
+                            properties: {
+                                name: { type: 'string' },
+                                type: { type: 'string' },
+                                ttl: { type: 'number' },
+                                disabled: { type: 'boolean' },
+                                content: { type: 'string' }
+                            }
+                        }
+                    ]
+                };
+
+                let result = inspector.validate(recordsSchema, records);
+
+                if (!result.valid) {
+                    err = new Error(`API returned invalid results: \n\n${result.format()}`);
+
+                    throw err;
+                }
+
+                return resolve(records);
+            })
+            .catch((e) => {
+                return reject(e);
+            });
+        });
     }
 }
 
---
+++ test/Records.test.js
@@ -0,0 +1,131 @@
+import chai, {expect} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import proxyquire from 'proxyquire';
+import Promise from 'bluebird';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('Records', () => {
+    const configuration = {
+        'host': 'abc',
+        'port': 8080,
+        'protocol': 'http',
+        'key': 'abcd'
+    };
+
+    let Connection,
+        Records;
+
+    let connection,
+        requestStub,
+        validRecord,
+        validServer,
+        validZone,
+        records;
+
+    before(() => {
+        Records = require('../lib/Records');
+
+        validServer = {
+            'type': 'Server',
+            'id': 'localhost',
+            'url': '/servers/localhost',
+            'daemon_type': 'authoritative',
+            'version': '3.4.7',
+            'config_url': '/servers/localhost/config{/config_setting}',
+            'zones_url': '/servers/localhost/zones{/zone}'
+        };
+
+        validRecord = {
+            'name': 'server.test.net',
+            'type': 'A',
+            'ttl': 3600,
+            'disabled': false,
+            'content': '192.168.0.1'
+        };
+
+        validZone = {
+            'id': 'test.net.',
+            'url': '/servers/localhost/zones/test.net.',
+            'name': 'test.net',
+            'kind': 'Master',
+            'dnssec': false,
+            'account': '',
+            'masters': [],
+            'serial': 1970010101,
+            'notified_serial': 0,
+            'last_check': 0,
+            'records': [ validRecord ]
+        };
+    });
+
+    beforeEach((done) => {
+        requestStub = sinon.stub();
+
+        Connection = proxyquire('../lib/Connection', {
+            'request': requestStub
+        });
+
+        requestStub.yields(null, null, [validServer]);
+
+        connection = new Connection(configuration);
+        records = new Records(connection);
+
+        connection.connect()
+        .then(() => {
+            done();
+        });
+    });
+
+    describe('constructor()', () => {
+        beforeEach(() => {
+            Records = require('../lib/Records');
+        });
+
+        it('should throw an error if not passed a Connection object', () => {
+            return expect(() => {
+                return new Records();
+            }).to.throw('Expected Connection');
+        });
+
+        it('should store the Connection object locally', () => {
+            let expectedConnection = {
+                'test': 'abcd'
+            };
+
+            let records = new Records(expectedConnection);
+
+            return expect(records.connection).to.deep.equal(expectedConnection);
+        });
+    });
+
+    describe('list()', () => {
+        it('should return a Promise', () => {
+            return expect(records.list()).to.be.an.instanceOf(Promise);
+        });
+
+        it('should reject if not passed a zone', () => {
+            requestStub.yields(null, null, null);
+
+            return expect(records.list()).to.eventually.be.rejectedWith('zone must be supplied');
+        });
+
+        it('should reject if the server connected to does not return an expected result', () => {
+            requestStub.yields(null, null, null);
+
+            return expect(records.list(validZone.id)).to.eventually.be.rejectedWith('API returned invalid results');
+        });
+
+        it('should resolve with a list of records', () => {
+            requestStub.yields(null, null, validZone);
+
+            return records.list(validZone.id)
+            .then((recordList) => {
+                return expect(recordList).to.deep.equal([validRecord]);
+            });
+        });
+    });
+});
